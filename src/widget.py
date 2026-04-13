1d707aceca2e22b6a7f8bcbecca77e327c9e8cbf
Anna Makarova <foxygoldy84@gmail.com>
2026-04-13 09:18:49 +0300

    mask_account

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,28 @@
-def mask_account_card():
+from black.nodes import Union
+
+
+def mask_account_card(data: str) -> str:
+    """Функция маскировки карты и счета"""
+    parts = data.split()
+    name = " ".join(parts[:-1])
+    number = parts[-1]
+
+    if not number.isdigit():
+        return "Ошибка, номер должен состоять только из цифр"
+
+    if name.lower().startswith("счет"):
+        if len(number) != 20:
+            return "Номер сета должен содержать 20 цифр"
+        masked_number = f"**{number[-4:]}"
+    else:
+        if len(number) != 16:
+            return "Номер карты должен содержать 16 цифр"
+        masked_number = f"**{number[:4] {number[4:6]}** **** {number[-4:]"
+                
+return f"{name.title()} {masked_number}"
+
+
+
+
+
+
