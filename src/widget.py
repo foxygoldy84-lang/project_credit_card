e1fbc2788c25a8d13a8625fa268a8542ad9f1a32
Anna Makarova <foxygoldy84@gmail.com>
2026-04-13 10:08:18 +0300

    widget_mypy

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-from black.nodes import Union
-
 
 def mask_account_card(data: str) -> str:
     """Функция маскировки карты и счета"""
@@ -14,24 +12,28 @@ def mask_account_card(data: str) -> str:
 
     if name.lower().startswith("счет"):
         if len(number) != 20:
-            return "Номер сета должен содержать 20 цифр"
+            return "Номер счета должен содержать 20 цифр"
         masked_number = f"**{number[-4:]}"
     else:
         if len(number) != 16:
             return "Номер карты должен содержать 16 цифр"
-        masked_number = f"**{number[:4] {number[4:6]}** **** {number[-4:]"
-                
-return f"{name.title()} {masked_number}"
+        masked_number = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
+
+    return f"{name.title()} {masked_number}"
 
 
 def get_data(data_string: str) -> str:
     """Преобразует строку в формат дд.мм.гггг"""
     try:
         data_iso = data_string[:10]
-        data_obj = datetime.strptime("%d.%m.%Y")
+        data_obj = datetime.strptime(data_iso, "%Y-%m-%d")
         return data_obj.strftime("%d.%m.%Y")
     except ValueError:
         return "Ошибка: некорректный ввод данных."
+
+
+
+
 
 
 
