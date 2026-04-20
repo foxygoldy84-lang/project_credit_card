abe42b3182f3dfc2fb907fa7ceda661c5c641d93
Anna Makarova <foxygoldy84@gmail.com>
2026-04-20 16:47:48 +0300

    linter check

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,14 @@
-from processing import filter_by_state, sort_by_date
-from widget import get_date, mask_account_card
-
-
-def filter_by_state(data, state='EXECUTED') -> str:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> str:
     """Функция  возвращает новый список по ключу 'state'"""
     new_state = []
     for i in data:
-        if i.get('state') == state:
+        if i.get("state") == state:
             new_state.append(i)
 
     return new_state
 
 
-def sort_by_date(data, reverse=True):
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
     """Функция сортирует список словарей по ключу 'date'"""
-    sorted_data = sorted(data, key=lambda x: x['date'], reverse=reverse)
+    sorted_data = sorted(data, key=lambda x: x["date"], reverse=reverse)
     return sorted_data
