a61af07b5c61e6dfa2b5cb98f1554b660ec400ae
Anna Makarova <foxygoldy84@gmail.com>
2026-04-21 09:40:23 +0300

    исправлены ошибки в папке src

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-def filter_by_state(data: list[dict], state: str = "EXECUTED") -> str:
-    """Функция  возвращает новый список по ключу 'state'"""
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция возвращает новый список по ключу 'state'"""
     new_state = []
     for item in data:
         if item.get("state") == state:
