b09def78071aad10cf812366b46c31315f6c417b
Anna Makarova <foxygoldy84@gmail.com>
2026-04-30 10:44:42 +0300

    Написание transaction_descriptions, card_number_generator плюс создание файла test_generator

--- generators/filter_by_currency.py
+++ generators/filter_by_currency.py
@@ -1,7 +1,7 @@
 from typing import Iterable, list, Dict, Any
 
 def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) ->  Iterable[Dikt[str, Any]]:
-    """ Генератор, который принимает список словарей с транзакцией и выдает с заданной валютой"""
+    """ Генератор, который принимает список словарей с транзакцией и выдает с заданной валютой. """
     for  transaction in transactions:
         current_currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
         if current_currency != currency:
@@ -9,3 +9,20 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) ->  It
 
         yield transaction
 
+
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterable[str]:
+    """ Генератор, который принимает список словарей с транзакциями
+        и поочередно возвращает описание каждой.
+    """
+    for transaction in transactions:
+        yield transaction.get("description", "Описание отсутствует")
+
+
+def card_number_generator(start: int, end: int) -> Iterаblе[str]:
+    """ Генератор, который создает номера карт в заданном формате."""
+    for number in range(start, end + 1):
+        number_str = f"{number:016}"
+        formatted_card = f"{number_str[:4]} {number_str[4:8]} {number_str[8:12]} {number_str[12:16]}
+
+        yield formatted_card
+        
