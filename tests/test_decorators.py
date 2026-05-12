f0c136b4bf8bd52df9bf275a23278671f095f04a
Anna Makarova <foxygoldy84@gmail.com>
2026-05-12 18:16:38 +0300

    дополнение README

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,25 +1,28 @@
-from ctypes import pydll
-from fileinput import filename
+import os
+from typing import Any
 
 import pytest
-import os
+from _pytest.capture import CaptureFixture
+
 from src.decorators import log
 
+
 # Тест 1: Успешное выполнение и вывод в консоль
-def test_log_console_ok(capsys):
+def test_log_console_ok(capsys: CaptureFixture) -> None:
     @log()
-    def my_function(x, y):
+    def my_function(x: int, y: int) -> int:
         return x + y
 
     my_function(1, 2)
     captured = capsys.readouterr()
     assert "my_function ok" in captured.out
 
-# Тест 2: Щшибка и вывод в консоль
-def test_log_console_error(capsys):
+
+# Тест 2: Ошибка и вывод в консоль
+def test_log_console_error(capsys: CaptureFixture) -> None:
     @log()
-    def my_function(x, y):
-        return x / 0
+    def my_function(x: int, y: int) -> float:
+        return x / y
 
     with pytest.raises(ZeroDivisionError):
         my_function(1, 0)
@@ -27,14 +30,15 @@ def test_log_console_error(capsys):
     captured = capsys.readouterr()
     assert "my_function error: ZeroDivisionError. Inputs: (1, 0), {}" in captured.out
 
-#Тест 3: Запись в файл при успешном выполнении
-def test_log_file_ok():
+
+# Тест 3: Запись в файл при успешном выполнении
+def test_log_file_ok() -> None:
     filename = "test_log.txt"
     if os.path.exists(filename):
         os.remove(filename)
 
     @log(filename=filename)
-    def my_function(x, y):
+    def my_function(x: int, y: int) -> int:
         return x + y
 
     my_function(1, 2)
@@ -42,18 +46,18 @@ def test_log_file_ok():
     with open(filename, "r") as f:
         log_content = f.read()
 
-    assert  "my_function ok" in log_content
+    assert "my_function ok" in log_content
     os.remove(filename)
 
 
 # Тест 4: Запись в файл при ошибке
-def test_log_file_error():
+def test_log_file_error() -> None:
     filename = "test_error_log.txt"
     if os.path.exists(filename):
         os.remove(filename)
 
     @log(filename=filename)
-    def my_function(x, y):
+    def my_function(x: int, y: int) -> Any:
         raise ValueError("test error")
 
     with pytest.raises(ValueError):
@@ -64,4 +68,3 @@ def test_log_file_error():
 
     assert "my_function error: ValueError. Inputs: (1, 2), {}" in log_content
     os.remove(filename)
-
